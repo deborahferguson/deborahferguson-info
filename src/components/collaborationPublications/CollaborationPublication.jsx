42bcde496c66651a85369eabd8b368c3dd2e2955
cameron <4129967+cameron-powell@users.noreply.github.com>
2026-04-25 12:33:49 -0400

    Include extra info in Collaboration Publication

--- src/components/collaborationPublications/CollaborationPublication.jsx
+++ src/components/collaborationPublications/CollaborationPublication.jsx
@@ -1,7 +1,7 @@
 import ViewArxivButton from "./ViewArxivButton"
 import ViewPublicationButton from "./ViewPublicationButton"
 
-export default function CollaborationPublication({ title, author, collaboration, arxivUrl, doiUrl, year }) {
+export default function CollaborationPublication({ title, author, collaboration, arxivUrl, doiUrl, year, involvementExplanation, outreachMaterialLinks }) {
 
     const renderTitle = (text) => {
         if (!text.includes("M_⊙")) {
@@ -28,14 +28,39 @@ export default function CollaborationPublication({ title, author, collaboration,
     return (
         <div className="my-1">
             <p className="font-semibold">{renderTitle(title)}</p>
+            
             <p className="text-sm">
                 {year}
                 <span className="ml-2 italic">
                     {collaboration ? collaboration : author}
                 </span>
             </p>
+            
+            {involvementExplanation && (
+                <p className="text-sm">
+                    {involvementExplanation}
+                </p>
+            )}
+            
             {doiUrl && <ViewPublicationButton doiUrl={doiUrl} />}
+            
             {arxivUrl && <ViewArxivButton arxivUrl={arxivUrl} />}
+            
+            {outreachMaterialLinks && outreachMaterialLinks.length > 0 && (
+                <div className="text-sm">
+                    {outreachMaterialLinks.map((link, index) => (
+                        <a
+                            key={index}
+                            href={link.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="inline-block mr-1 px-1.5 py-1 bg-slate-300 hover:bg-slate-400 text-slate-700 rounded-md text-xs"
+                        >
+                            {link.title}
+                        </a>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
