b985459c0453d10150490f8cf4bfa39b00b7855e
cameron <4129967+cameron-powell@users.noreply.github.com>
2026-04-25 13:35:47 -0400

    Remove scaling of publications -- not interactable

--- src/components/shortAuthorPublications/ShortAuthorPublication.jsx
+++ src/components/shortAuthorPublications/ShortAuthorPublication.jsx
@@ -6,7 +6,7 @@ import { ABSTRACTS_BY_ARXIV_URL } from "../../data/shortAuthorPublications"
 export default function ShortAuthorPublication({ title, author, journal, doiUrl, arxivUrl }) {
     return (
         <div
-            className="border border-slate-300 bg-white rounded-xs text-center h-70 px-4 sm:px-8 md:px-12 lg:px-16 py-8 hover:scale-101 overflow-hidden flex flex-col"
+            className="border border-slate-300 bg-white rounded-xs text-center h-70 px-4 sm:px-8 md:px-12 lg:px-16 py-8 overflow-hidden flex flex-col"
         >
             <p className="text-md font-semibold mt-1">{title}</p>
             <p className="text-xs mt-4 mb-2">{author}</p>
